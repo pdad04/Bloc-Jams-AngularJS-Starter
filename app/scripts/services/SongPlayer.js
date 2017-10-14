fae6763fc2cf3170dc711c60e500f8493cafb028
Andre Dean <Andre@Andres-Retina-MacBook-Pro.local>
2017-10-14 07:09:54 -0700

    Add private playSong function and add documentation

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
   function SongPlayer() {
     var SongPlayer = {};
 
+    /**
+    * @desc Song object
+    * @type {object}
+    */
     var currentSong = null;
 
     /**
@@ -29,22 +33,42 @@
       currentSong = song;
     };
 
+    /**
+    * @function playSong
+    * @desc play song and set playing to true
+    * @param {object} song
+    */
+    var playSong = function(song){
+      currentBuzzObject.play();
+      song.playing = true;
+    }
+
+    /**
+    * @function play
+    * @desc if currentSong is not song calls setSong and playSong. Otherwise checks if currentBuzzObject isPaused, if so calls playSong
+    * @param {object} song
+    */
     SongPlayer.play = function(song) {
       if(currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if(currentSong === song) {
           if(currentBuzzObject.isPaused()) {
-            currentBuzzObject.play();
+            playSong(song);
           }
       }
     };
 
+    /**
+    * @function pause
+    * @desc pauses song and sets playing to false
+    * @param {object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
     };
+    
     return SongPlayer;
   }
 
